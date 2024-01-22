e1fdd593ebcc4cdb3e012bcf76f14a872d02784a
josunghoon <whtjdgns0029@naver.com>
2024-01-22 18:54:17 +0900

    feat: SpringBoot - Load Image

--- src/main/java/com/project/web/dto/response/board/BoardDetailResponse.java
+++ src/main/java/com/project/web/dto/response/board/BoardDetailResponse.java
@@ -1,12 +1,17 @@
 package com.project.web.dto.response.board;
 
-import com.project.web.dto.response.file.FileUploadResponse;
 import com.project.web.entity.Board;
+import com.project.web.entity.File;
+import com.project.web.service.FileService;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.util.CollectionUtils;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -16,17 +21,37 @@ public class BoardDetailResponse {
     private String contents;
     private int hits;
     private String member;
-    private List<FileUploadResponse> files;
+    private List<String> fileUrls;
+    private List<byte[]> fileBytes;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public static BoardDetailResponse toDTO(Board board) {
+        List<String> fileUrls = board.getFiles()
+                .stream()
+                .map(File::getFilePath)
+                .collect(Collectors.toList());
+
+        List<byte[]> fileBytes = new ArrayList<>();
+
+        if (!CollectionUtils.isEmpty(board.getFiles())) {
+            for (File file : board.getFiles()) {
+                try {
+                    byte[] bytes = FileService.loadImageAsByteArray(file.getFilePath());
+                    fileBytes.add(bytes);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
         return BoardDetailResponse.builder()
                 .title(board.getTitle())
                 .contents(board.getContents())
                 .hits(board.getHits())
                 .member(board.getMember().getName())
-                .files(FileUploadResponse.toDTO(board.getFiles()))
+                .fileUrls(fileUrls)
+                .fileBytes(fileBytes)
                 .createdAt(board.getCreatedAt())
                 .updatedAt(board.getUpdatedAt())
                 .build();
--- src/main/java/com/project/web/service/FileService.java
+++ src/main/java/com/project/web/service/FileService.java
@@ -12,6 +12,9 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 
@@ -48,5 +51,10 @@ public class FileService {
                 }
             }
         }
+    }
+
+    public static byte[] loadImageAsByteArray(String filePath) throws IOException {
+        Path path = Paths.get(filePath);
+        return Files.readAllBytes(path);
     }
 }
